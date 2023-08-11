c55412ab7da444782c38a79ca5529ef2cd5ff9c2
Houda0612 <hou.da090601@example.com>
2023-08-11 10:44:04 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    // Use the puts function to print the desired text
+    puts("Programming is like building a multilingual puzzle,");
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
